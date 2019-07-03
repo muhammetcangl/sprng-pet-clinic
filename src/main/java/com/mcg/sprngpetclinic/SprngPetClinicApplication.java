95a3d9eccbfe77387d64e2dd38ca164f2d809c6c
Muhammetcan Gul <muhammetcangl@gmail.com>
2019-07-03 12:36:58 +0300

    initial commit

---
+++ src/main/java/com/mcg/sprngpetclinic/SprngPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.mcg.sprngpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprngPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprngPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcg/sprngpetclinic/SprngPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mcg.sprngpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SprngPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
